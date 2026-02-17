cb5159cd1d983e7a60aa53cab8bfc5281a93544c
William Wei Xu <64020697+dbrices@users.noreply.github.com>
2026-02-17 18:23:09 -0500

    Create name_length.py

---
+++ name_length.py
@@ -0,0 +1,5 @@
+string_text = input("Enter your user's full name: ")
+length_value = len(string_text)
+print("Length:", length_value, "Length Value Type:", type(length_value), "String Text Type:", type(string_text))
+
+
